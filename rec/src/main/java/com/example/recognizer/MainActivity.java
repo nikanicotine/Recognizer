fa8e40ec05c5551f2d676852f0c9685f8f1b67d6
Вероника Скирдова <verka0222@gmail.com>
2023-12-09 23:28:34 +0300

    Initial commit

---
+++ rec/src/main/java/com/example/recognizer/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.recognizer;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+public class MainActivity extends Activity {
+    Button mButton;
+    private TextView EnteredText;
+    private static final int Print_Words = 100;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+
+        EnteredText = findViewById(R.id.tv);
+        mButton = findViewById(R.id.button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Ну и что ты ему сказал?");
+                startActivityForResult(intent, Print_Words);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Print_Words && resultCode == RESULT_OK) {
+            ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            EnteredText.setText(result.toString());
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
